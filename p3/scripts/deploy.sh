aa52a5841a45084c1c26ad36a3ae5bc2a2118e8c
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-12-24 10:49:05 -0300

    feat: deploy will-playground application

--- p3/scripts/deploy.sh
+++ p3/scripts/deploy.sh
@@ -31,10 +31,22 @@ kubectl apply -k ../confs/argocd/
 kubectl rollout status deployment/argocd-server -n argocd
 
 kubectl apply -f ../confs/argocd/ingress.yaml
+sleep 0.5
+echo "argoCD deployed"
 
 ### argoCD Configuration###
-echo "argoCD deployed, starting configuration"
+echo "Starting argoCD configuration"
 
+echo "Loggin in with the default passord..."
 DEFAULT_PASSWD=$(argocd admin initial-password -n argocd | head -1)
-argocd login localhost --username admin --password "$DEFAULT_PASSWD" --insecure --grpc-web
+argocd login argocd.localhost --username admin --password "$DEFAULT_PASSWD" --insecure --grpc-web
+
+echo "Changing the default password"
 argocd account update-password --account admin --current-password "$DEFAULT_PASSWD" --new-password "$NEW_PASSWD" --grpc-web
+
+echo "Configuring deployed application"
+kubectl create namespace dev
+kubectl config set-context --current --namespace=argocd
+argocd app create will-playground --repo https://github.com/gabrielclcardoso/gcorreia_argocd_application --path . --dest-server https://kubernetes.default.svc --dest-namespace dev
+argocd app sync will-playground 
+argocd app set will-playground --sync-policy automated
